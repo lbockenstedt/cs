cb92ae3c22231767f16164a05e4f552af978fd21
Lance Bockenstedt <lrb@hpe.com>
2026-06-09 21:55:07 -0500

    Auto-sync: Static analysis verified build 2026-06-09 21:55

--- src/cs_spoke.py
+++ src/cs_spoke.py
@@ -1,6 +1,6 @@
 import logging
 from typing import Dict, Any
-from base_spoke import BaseSpoke
+from lm.core.src.base_spoke import BaseSpoke
 from .simulation_engine import SimulationEngine
 
 logger = logging.getLogger("CSSpoke")
